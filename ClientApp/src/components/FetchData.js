620d693c0fcb9cb66e806b00a086b8927d738b09
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-07-20 16:15:07 -0700

    fetches API with react hooks
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -1,64 +1,37 @@
 import React from 'react';
 
-export default () => {
-  renderForecastsTable = (forecasts) => {
-    return (
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
-  }
+const FetchData = () => {
+  let [forecastData, populateForecastData] = React.useState([]);
 
-  // formerly async function needs useEffects
-  populateWeatherData = () => {
-    const response = await fetch('weatherforecast');
-    const data = await response.json();
-    this.setState({ forecasts: data, loading: false });
-  }
+  const fetchApi = React.useCallback(() => {
+    fetch(`http://localhost:5000/api/Animals`, 
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(response => response.json())
+    .then(response => populateForecastData(response))
+    .catch(error => console.log(error))
+  }, [])
 
-  // let contents = this.state.loading
-  //   ? <p><em>Loading...</em></p>
-  //   : FetchData.renderForecastsTable(this.state.forecasts);
+  React.useEffect(() => {
+    fetchApi()
+  }, [fetchApi])
 
   return (
     <div>
       <h1 id="tabelLabel" >Weather forecast</h1>
       <p>This component demonstrates fetching data from the server.</p>
-      {contents}
+      {Object.values(forecastData).map(dino => {
+        return <ul>
+          <li>Dino name: {dino.name} :: Species: {dino.species} :: Age {dino.age} :: Gender: {dino.gender}</li>
+        </ul>
+      })}
+      <button type='button' onClick={fetchApi}>Fetch data again</button>
     </div>
   );
 }
 
-
-
-
-// export class FetchData extends Component {
-//   static displayName = FetchData.name;
-
-//   constructor(props) {
-//     super(props);
-//     this.state = { forecasts: [], loading: true };
-//   }
-
-//   componentDidMount() {
-//     this.populateWeatherData();
-//   }
-
-//   static 
+export default FetchData
